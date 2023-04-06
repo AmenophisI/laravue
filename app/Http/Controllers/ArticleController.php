d6f72d75c7977dd8f0690670f3518509e99d2672
ryosuke <sessyomaru427@gmail.com>
2023-04-06 21:07:40 +0900

    記事一覧作成

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    //
+    public function index(){
+        $articles = Article::all()->sortByDesc('created_at');
+        return view('articke.index', compact('articles'));
+    }
 }
